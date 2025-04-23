bcb52642b934571aef415c605e53822122f324e9
Utsav Choudhary <uchoudhary2005@hotmail.com>
2025-04-23 18:32:45 +0530

    Assembled APIs for historical data fetching

--- src/config/api.js
+++ src/config/api.js
@@ -7,5 +7,6 @@ export const SingleCoin = (id) =>
 export const HistoricalChart = (id, days = 365, currency) =>
   `/api/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
 
+
 export const TrendingCoins = (currency, perPage = 6, page = 1) =>
   `/api/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=24h`;
