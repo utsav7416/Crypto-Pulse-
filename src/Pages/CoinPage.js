9bf06e5d13ffc802c486dbeba50f4f5d8e8ad0b2
Utsav Choudhary <uchoudhary2005@hotmail.com>
2025-06-01 22:09:09 +0530

    Kurtosis and GARCH plots, coin info, Gecko Historical Data updates

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -29,7 +29,7 @@ const CoinPage = () => {
 
   const fetchPrediction = async () => {
     try {
-      const res = await axios.get(`http://localhost:5001/predict/${id}`);
+      const res = await axios.get(`/predict/${id}`);
       setPredictionData(res.data);
     } catch (error) {
       console.error("Error fetching prediction data:", error);
